c832670d71c131c836471df258ddd6d8d6443f0b
Andreu Gallofré <andreugallofre@gmail.com>
2018-05-27 23:38:09 +0200

    Create P14952.cc

---
+++ P14952.cc
@@ -0,0 +1,46 @@
+/* Si el graf té un cicle l'algorisme para pero el programa no explota
+Un graf dirigit que té un cicle, no té ordenació topologica, ens serveis
+per veure si un graf té cicles o no*/
+
+#include <queue>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+using VE = vector<int>;
+using ME = vector<VE>;
+
+int main() {
+    int n,m;
+    while (cin >> n >> m) {
+        
+        ME G(n);
+        VE grau(n,0);
+
+        for (int r = 0; r < m; ++r) {
+            int x,y;
+            cin >> x >> y;
+            G[x].push_back(y);
+            ++grau[y];
+        }
+
+        priority_queue<int> pq;
+
+        for(int x = 0; x < n; ++x)
+            if (grau[x] == 0) pq.push(-x);
+       
+        bool primer = true;
+
+        while(not pq.empty()) {
+            int x = pq.top(); pq.pop();
+
+            if(primer){ cout << -x; primer = false;}
+            else cout << " " << -x;
+
+            for (int y : G[-x])
+                if (--grau[y] == 0) pq.push(-y);
+        }
+        cout << endl;
+    }
+}
